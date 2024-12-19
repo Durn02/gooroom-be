6952115cbff527122acfdd173b3cb94c1f960323
Durn02 <engus9902@gmail.com>
2024-12-19 16:26:21 +0900

    Fix: min_length 제한 없앰

--- domain/service/content/request/create_sticker_request.py
+++ domain/service/content/request/create_sticker_request.py
@@ -3,5 +3,5 @@ from typing import List
 
 
 class CreateStickerRequest(BaseModel):
-    content: str = Field(..., min_length=1)
+    content: str = Field(..., min_length=0)
     image_url: List[str]
